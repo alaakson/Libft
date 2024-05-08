d58442da3ab02b7670d627c48ffdf0695529ee9c
Anton  Laaksonen <alaakson@c1r6p20.hive.fi>
2024-05-08 13:39:01 +0300

    small fixes 8.5

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: alaakson <alaakson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 08:06:43 by alaakson          #+#    #+#             */
-/*   Updated: 2024/05/03 12:28:26 by alaakson         ###   ########.fr       */
+/*   Updated: 2024/05/08 12:29:23 by alaakson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@ void	*ft_memset(void *b, int c, size_t len)
 	unsigned char	*tmp;
 
 	tmp = (unsigned char *) b;
-	while (len > 0)
+	while (0 < len)
 	{
-		*(tmp++) = (unsigned char) c;
+		*(tmp) = (unsigned char) c;
+		tmp++;
 		len--;
 	}
 	return (b);
