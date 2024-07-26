d163754e61eff17606f9fe43d553074c7aa5c2be
alaakson <162610032+alaakson@users.noreply.github.com>
2024-07-26 17:11:34 +0300

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -17,9 +17,11 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	i;
 
 	i = 0;
-	while (s[i])
+	if (!s || !f)
+		return ;
+	while (s[i] != '\0')
 	{
-		(*f)(i, &s[i]);
+		f(i, &s[i]);
 		i++;
 	}
 }
