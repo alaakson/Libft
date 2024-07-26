b19d4636d21ee45d5165982dcd132132c283cc94
alaakson <162610032+alaakson@users.noreply.github.com>
2024-07-26 17:11:15 +0300

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -39,3 +39,31 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	dest[d] = '\0';
 	return (dest);
 }
+
+/// ++ added inside dest and s1 and s1
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	int		d;
+	int		s;
+
+	s = 0;
+	d = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	dest = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!dest)
+		return (NULL);
+	while (s1[s])
+	{
+		dest[d++] = s1[s++];
+	}
+	s = 0;
+	while (s2[s])
+	{
+		dest[d++] = s2[s++];
+	}
+	dest[d] = '\0';
+	return (dest);
+}
