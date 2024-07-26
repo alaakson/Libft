b908ab7e81513232ad1e2dead737f297244e6c09
alaakson <162610032+alaakson@users.noreply.github.com>
2024-07-26 17:10:14 +0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,28 +14,25 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	j;
-	size_t	i;
 	size_t	dst_len;
 	size_t	src_len;
+	size_t	j;
+	size_t	i;
 
-	j = 0;
-	dst_len = ft_strlen(dst);
+	if (!dst && !src)
+		return (0);
 	src_len = ft_strlen(src);
-	i = dst_len;
 	if (dstsize == 0)
 		return (src_len);
-	if (dstsize < dst_len)
+	dst_len = ft_strlen(dst);
+	if (dst_len >= dstsize)
 		return (src_len + dstsize);
-	while (src[j] && (dst_len + j) < dstsize)
+	i = dst_len;
+	j = 0;
+	while (src[j] && (i < dstsize - 1))
 	{
-		dst[i] = src[j];
-		i++;
-		j++;
+		dst[i++] = src[j++];
 	}
-	if ((dst_len + j) == dstsize && dst_len < dstsize)
-		dst[--i] = '\0';
-	else
-		dst[i] = '\0';
-	return (src_len + dst_len);
+	dst[i] = '\0';
+	return (dst_len + src_len);
 }
